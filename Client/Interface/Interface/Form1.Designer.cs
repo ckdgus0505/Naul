e34735d4930f3cc1fd3476a922b7eba951fe34a4
ckdgus0505 <ckdgus0505@gmail.com>
2019-05-29 15:44:37 +0900

    add client

---
+++ Client/Interface/Interface/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btn_att_start = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_spk_start = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_att_end = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btn_att_start,
+            this.btn_spk_start,
+            this.btn_att_end});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1968, 38);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btn_att_start
+            // 
+            this.btn_att_start.Name = "btn_att_start";
+            this.btn_att_start.Size = new System.Drawing.Size(158, 34);
+            this.btn_att_start.Text = "출석 시작하기";
+            this.btn_att_start.Click += new System.EventHandler(this.Btn_att_start_Click);
+            // 
+            // btn_spk_start
+            // 
+            this.btn_spk_start.Name = "btn_spk_start";
+            this.btn_spk_start.Size = new System.Drawing.Size(158, 34);
+            this.btn_spk_start.Text = "호명 출석하기";
+            this.btn_spk_start.Click += new System.EventHandler(this.Btn_spk_start_Click);
+            // 
+            // btn_att_end
+            // 
+            this.btn_att_end.Name = "btn_att_end";
+            this.btn_att_end.Size = new System.Drawing.Size(158, 34);
+            this.btn_att_end.Text = "출석 종료하기";
+            this.btn_att_end.Click += new System.EventHandler(this.Btn_att_end_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1298, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(658, 720);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBoxIpl1.TabIndex = 4;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1968, 771);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "화상출결시스템";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btn_att_start;
+        private System.Windows.Forms.ToolStripMenuItem btn_att_end;
+        private System.Windows.Forms.ToolStripMenuItem btn_spk_start;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+    }
+}
+
---
+++ Client/Interface/Interface/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using System.Net;
+using System.Net.Sockets;
+using MySql.Data.MySqlClient;
+
+namespace Interface
+{
+    public partial class Form1 : Form
+    {
+        byte[] receive = new Byte[1024];
+
+        string IP = "175.214.125.27";
+        Int32 PORT = 9009;
+        bool checking = false; // 사람이 인식되고 있음을 나타내는 변수
+
+
+
+        IplImage haarface;
+        CvCapture capture;
+        IplImage src;
+        public Form1()
+        {
+            InitializeComponent();
+            //updateDataGridView();
+            DBConnection();
+        }
+
+
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void updateDataGridView()
+        {
+            //string[] row0 = { "01", "아몰랑" };
+            //dataGridView1.Rows.Add(row0);
+            //dataGridView1.Rows.Add("02", "정창현", true);
+            //dataGridView1.Rows.Add("03", "창");
+            //dataGridView1.Rows.Add("04", "현");
+            //dataGridView1.Rows.Add("05", "이");
+            //dataGridView1.Rows.Add("06", "동");
+            //dataGridView1.Rows.Add("07", "석");
+            //dataGridView1.Rows.Add("08", "임");
+            //dataGridView1.Rows.Add("09", "상");
+            //dataGridView1.Rows.Add("10", "균");
+            //dataGridView1.Rows.Add("11", "박");
+            //dataGridView1.Rows.Add("12", "현");
+            //dataGridView1.Rows.Add("13", "준");
+            //dataGridView1.Rows.Add("14", "오");
+            //dataGridView1.Rows.Add("15", "현");
+            //dataGridView1.Rows.Add("16", "우");
+            //dataGridView1.Rows.Add("17", "임");
+            //dataGridView1.Rows.Add("18", "현");
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            src = capture.QueryFrame();
+
+            pictureBoxIpl1.ImageIpl = FaceDetection(src);
+        }
+
+        private void camera_On()
+        {
+            try
+            {
+                capture = CvCapture.FromCamera(CaptureDevice.DShow, 0);
+                capture.SetCaptureProperty(CaptureProperty.FrameWidth, 640);
+                capture.SetCaptureProperty(CaptureProperty.FrameHeight, 360);
+
+
+            }
+            catch
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void camera_Off()
+        {
+            //CvReleaseFunc(&capture);
+        }
+
+        private void Btn_att_start_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("출석을 시작합니다.");
+            camera_On();
+            timer1.Enabled = true;
+
+
+
+
+        }
+
+        private void Btn_spk_start_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("출석을 시작합니다.");
+            DBConnection();
+
+        }
+
+        private void Btn_att_end_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = false;
+            // while(true)
+            //{
+            //    send_number();
+            //}
+        }
+
+
+        private void send_number() // 학번, 학수번호를 보내서 출석을 함
+        {
+            int length = 0;
+            Socket sock = connection();
+
+            try
+            {
+                Byte[] data = Encoding.Default.GetBytes("201511041ea0017_1");
+                sock.Send(data);
+
+                length = sock.Receive(receive);
+
+                MessageBox.Show(Encoding.UTF8.GetString(receive));
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+            finally
+            {
+                sock.Close();
+            }
+        }
+
+        private void send_class() // 학수번호를 보내서 해당 과목의 학생들을 받음  => DB 직접 접속해서 받아올것!
+        {
+            int length = 0;
+            Socket sock = connection();
+
+            try
+            {
+
+                Byte[] data = Encoding.Default.GetBytes("ea0017_1");
+                sock.Send(data);
+
+                length = sock.Receive(receive);
+
+                MessageBox.Show(Encoding.UTF8.GetString(receive));
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+            finally
+            {
+                sock.Close();
+            }
+        }
+
+
+        private void send_picture() // 사진을 보내서 해당하는 학번을 받음
+        {
+            int length = 0;
+            Socket sock = connection();
+
+            try
+            {
+                byte[] pic = src.ToBytes(".jpg");
+
+                sock.Send(pic);
+
+                length = sock.Receive(receive);
+
+
+                MessageBox.Show(Encoding.UTF8.GetString(receive));
+
+                if(Encoding.UTF8.GetString(receive) == "undefined") // 사람 인식 못함
+                {
+                    checking = false;
+                }
+                else // 사람 인식 함
+                {
+
+
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+            finally
+            {
+                sock.Close();
+            }
+
+        }
+        private Socket connection()
+        {
+            //1. 접속할 종단점(서버측 소켓)생성
+            IPAddress ip = IPAddress.Parse(IP);//인자값 : 서버측 IP
+            IPEndPoint endPoint = new IPEndPoint(ip, PORT);//인자값 : IPAddress,포트번호
+
+            //2. Tcp Socket 생성
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //3. 접속(전화걸기)
+            sock.Connect(endPoint);
+
+
+            return sock;
+        }
+        private void DBConnection()
+        {
+            string constring = "datasource=isg1031.iptime.org;port=20050;username=aitester;password=aitester001!";
+            MySqlConnection connection = new MySqlConnection(constring);
+
+            MySqlCommand cmdDataBase = new MySqlCommand(" SELECT * FROM ai.client_table  ;", connection);
+
+            try
+            {
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmdDataBase;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource BSource = new BindingSource();
+
+                BSource.DataSource = dbdataset;
+                dataGridView1.DataSource = BSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }           
+            connection.Close();
+        }
+
+        private void SendPIC()
+        {
+        }
+
+        private void SendNum()
+        {
+        }
+
+        private void SendClass()
+        { }
+
+        public IplImage FaceDetection(IplImage src)
+        { // https://076923.github.io/posts/C-opencv-29/
+            haarface = new IplImage(src.Size, BitDepth.U8, 3); // harrface 는 원본을 복사한 이미지
+            Cv.Copy(src, haarface);
+
+            const double scale = 0.9; // scale은 검출되는 이미지의 비율
+            const double scaleFactor = 1.139; // 얼굴 검출시에 사용되는 상수
+            const int minNeighbors = 1; // 얼굴 검출시에 사용되는 상수
+
+            using (IplImage Detected_image = new IplImage(new CvSize(Cv.Round(src.Width / scale), Cv.Round(src.Height / scale)), BitDepth.U8, 1))
+            { // 검출되는 이미지인 detected image 를 scale의 비율에 맞게 재조정 함
+                using (IplImage gray = new IplImage(src.Size, BitDepth.U8, 1)) // 이미지의 크기를 조정
+                {
+                    Cv.CvtColor(src, gray, ColorConversion.BgrToGray);
+                    Cv.Resize(gray, Detected_image, Interpolation.Linear);
+                    Cv.EqualizeHist(Detected_image, Detected_image); // 이미지의 화상을 평탄화 (어둡고 밝은 부분이 조정됨)
+                }
+
+                using (CvHaarClassifierCascade cascade = CvHaarClassifierCascade.FromFile("../../haarcascade_frontalface_alt.xml"))
+                using (CvMemStorage storage = new CvMemStorage()) // 메모리에 저장소 생성
+                {
+                    CvSeq<CvAvgComp> faces = Cv.HaarDetectObjects(Detected_image, cascade, storage, scaleFactor, minNeighbors, HaarDetectionType.FindBiggestObject, new CvSize(90, 90), new CvSize(0, 0));
+                    // detected_image = 탐지할 이미지, cascade =  storage = 메모리가 저장될 저장소 , HarrDetectionType : 작동 모드 
+                    if (faces.Total == 1 && checking == false)
+                    {
+                        checking = true;
+                        MessageBox.Show("인식?"); //send_picture();
+    
+                    }
+                    for (int i = 0; i < faces.Total; i++)
+                    {
+                        CvRect r = faces[i].Value.Rect;
+                        CvPoint center = new CvPoint
+                        {
+                            X = Cv.Round((r.X + r.Width * 0.5) * scale),
+                            Y = Cv.Round((r.Y + r.Height * 0.5) * scale)
+                        };
+                        int radius = Cv.Round((r.Width + r.Height) * 0.25 * scale);
+                        haarface.Circle(center, radius, CvColor.Black, 3, LineType.AntiAlias, 0);
+                    }
+                }
+
+                return haarface;
+            }
+        }
+
+
+        public void loadDataGridView()
+        {
+            string constring = "datasource=localhost;port=3306;username=root;password=mimo";
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDataBase = new MySqlCommand(" select * from database.edata ;", conDataBase); // edata :db 이름
+
+            try
+            {
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmdDataBase;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource BSource = new BindingSource();
+
+                BSource.DataSource = dbdataset;
+                dataGridView1.DataSource = BSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
