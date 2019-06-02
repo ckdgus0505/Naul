44997164b8ac97833c9393e125389fb355026b3e
ImSangKyun <30777239+ImSangKyun@users.noreply.github.com>
2019-06-02 20:00:29 +0900

    Client_Program Upload

---
+++ 상균/Client_Program_C++++/Interface/Interface/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using System.Net;
+using System.Net.Sockets;
+using MySql.Data.MySqlClient;
+
+namespace Interface
+{
+    public partial class Form1 : Form
+    {
+        byte[] receive = new Byte[1024];
+        
+
+        IplImage haarface;
+        CvCapture capture;
+        IplImage src;
+        bool checking = false; // 사람이 인식되고 있음을 나타내는 변수
+
+        public Form1()  //main
+        {
+            InitializeComponent();
+        }
+
+        //
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e){}
+        private void Panel1_Paint(object sender, PaintEventArgs e){}
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e){}
+        private void SendPIC() { }
+        private void SendNum() { }
+        private void SendClass() { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }
+        //
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            src = capture.QueryFrame();
+
+            pictureBoxIpl1.ImageIpl = FaceDetection(src);
+        }
+
+        private void camera_On()
+        {
+            try
+            {
+                capture = CvCapture.FromCamera(CaptureDevice.DShow, 0);
+                capture.SetCaptureProperty(CaptureProperty.FrameWidth, 640);
+                capture.SetCaptureProperty(CaptureProperty.FrameHeight, 360);
+            }
+            catch
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void camera_Off()
+        {
+            //CvReleaseFunc(&capture);
+        }
+
+        private void Btn_att_start_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("출석을 시작합니다.");
+            dataGridView1.Rows.Clear();
+            DBConnection();
+            dataGridView1.Columns[3].ReadOnly = true;
+
+            camera_On();
+            timer1.Enabled = true;
+        }
+
+        private void Btn_spk_start_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("출석을 시작합니다.");
+            dataGridView1.Rows.Clear();
+            DBConnection();
+            dataGridView1.Columns[3].ReadOnly = false;
+        }
+
+        private void Btn_att_end_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = false;
+            // while(true)
+            //{
+            //    send_number();
+            //}
+        }
+
+
+        private void send_number() // 학번, 학수번호를 보내서 출석을 함
+        {
+            int length = 0;
+            Socket sock = connection();
+
+            try
+            {
+                Byte[] data = Encoding.Default.GetBytes("201511041ea0017_1");
+                sock.Send(data);
+
+                length = sock.Receive(receive);
+
+                MessageBox.Show(Encoding.UTF8.GetString(receive));
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+            finally
+            {
+                sock.Close();
+            }
+        }
+
+        private void send_class() // 학수번호를 보내서 해당 과목의 학생들을 받음  => DB 직접 접속해서 받아올것!
+        {
+            int length = 0;
+            Socket sock = connection();
+
+            try
+            {
+
+                Byte[] data = Encoding.Default.GetBytes("ea0017_1");
+                sock.Send(data);
+
+                length = sock.Receive(receive);
+
+                MessageBox.Show(Encoding.UTF8.GetString(receive));
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+            finally
+            {
+                sock.Close();
+            }
+        }
+
+
+        private void send_picture() // 사진을 보내서 해당하는 학번을 받음
+        {
+            int length = 0;
+            Socket sock = connection();
+            try
+            {
+                byte[] pic = src.ToBytes(".jpg");
+                sock.Send(pic);
+                length = sock.Receive(receive);
+
+                MessageBox.Show(Encoding.UTF8.GetString(receive));
+                if(Encoding.UTF8.GetString(receive) == "undefined") // 사람 인식 못함
+                {
+                    checking = false;
+                }
+                else // 사람 인식 함
+                {
+
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+            finally
+            {
+                sock.Close();
+            }
+
+        }
+        private Socket connection()
+        {
+            //1. 접속할 종단점(서버측 소켓)생성
+            IPAddress ip = Dns.GetHostAddresses("isg1031.iptime.org")[0];//인자값 : 서버측 IP
+            IPAddress ip2 = IPAddress.Parse("127.0.0.1");
+            IPAddress ip3 = IPAddress.Parse("36.39.21.137");
+            Int32 PORT = 20060;
+            IPEndPoint endPoint = new IPEndPoint(ip3, PORT);//인자값 : IPAddress,포트번호
+
+            //2. Tcp Socket 생성
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //3. 접속(전화걸기)
+            sock.Connect(endPoint);
+            return sock;
+        }
+
+        private void DBConnection()
+        {
+            string constring = "Server=isg1031.iptime.org;port=20050;Database=ai;username=aitester;password=aitester001!";
+            MySqlConnection connection = new MySqlConnection(constring);    //DB연결객체
+            MySqlCommand cmdDataBase = new MySqlCommand(" SELECT * FROM attendance;", connection);
+
+            try
+            {
+                connection.Open();
+
+                using (MySqlDataReader reader = cmdDataBase.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string[] row = { reader[0].ToString(), reader[1].ToString(), reader[3].ToString(), reader[2].ToString() };
+                        dataGridView1.Rows.Add(row);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }           
+            connection.Close();
+        }
+
+        public IplImage FaceDetection(IplImage src)
+        { // https://076923.github.io/posts/C-opencv-29/
+            haarface = new IplImage(src.Size, BitDepth.U8, 3); // harrface 는 원본을 복사한 이미지
+            Cv.Copy(src, haarface);
+
+            const double scale = 0.9; // scale은 검출되는 이미지의 비율
+            const double scaleFactor = 1.139; // 얼굴 검출시에 사용되는 상수
+            const int minNeighbors = 1; // 얼굴 검출시에 사용되는 상수
+
+            using (IplImage Detected_image = new IplImage(new CvSize(Cv.Round(src.Width / scale), Cv.Round(src.Height / scale)), BitDepth.U8, 1))
+            { // 검출되는 이미지인 detected image 를 scale의 비율에 맞게 재조정 함
+                using (IplImage gray = new IplImage(src.Size, BitDepth.U8, 1)) // 이미지의 크기를 조정
+                {
+                    Cv.CvtColor(src, gray, ColorConversion.BgrToGray);
+                    Cv.Resize(gray, Detected_image, Interpolation.Linear);
+                    Cv.EqualizeHist(Detected_image, Detected_image); // 이미지의 화상을 평탄화 (어둡고 밝은 부분이 조정됨)
+                }
+
+                using (CvHaarClassifierCascade cascade = CvHaarClassifierCascade.FromFile("../../haarcascade_frontalface_alt.xml"))
+                using (CvMemStorage storage = new CvMemStorage()) // 메모리에 저장소 생성
+                {
+                    CvSeq<CvAvgComp> faces = Cv.HaarDetectObjects(Detected_image, cascade, storage, scaleFactor, minNeighbors, HaarDetectionType.FindBiggestObject, new CvSize(90, 90), new CvSize(0, 0));
+                    // detected_image = 탐지할 이미지, cascade =  storage = 메모리가 저장될 저장소 , HarrDetectionType : 작동 모드 
+                    if (faces.Total == 1 && checking == false)
+                    {
+                        checking = true;
+                        //MessageBox.Show("인식?"); //send_picture();
+                        send_picture();
+                    }
+                    for (int i = 0; i < faces.Total; i++)
+                    {
+                        CvRect r = faces[i].Value.Rect;
+                        CvPoint center = new CvPoint
+                        {
+                            X = Cv.Round((r.X + r.Width * 0.5) * scale),
+                            Y = Cv.Round((r.Y + r.Height * 0.5) * scale)
+                        };
+                        int radius = Cv.Round((r.Width + r.Height) * 0.25 * scale);
+                        haarface.Circle(center, radius, CvColor.Black, 3, LineType.AntiAlias, 0);
+                    }
+                }
+
+                return haarface;
+            }
+        }
+
+        
+    }
+}
